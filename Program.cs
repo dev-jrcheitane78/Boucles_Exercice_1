5a7f1b2a61d057583d616e6cda43dbf266f5ba79
dev-jrcheitane <66439164+dev-jrcheitane@users.noreply.github.com>
2021-02-16 12:32:38 +0100

    Exrecice sur les boucles (while)

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boucle_Exercice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int compteur = 0;
+
+            while (compteur < 10)
+            {
+                Console.WriteLine("Bonjour, je ne suis qu'un simple message.");
+                compteur++;
+            }
+
+            Console.Read();
+        }
+    }
+}
